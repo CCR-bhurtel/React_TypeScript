be0fdf3aca43f96a73625d9f9d06062718a28225
cceer <sisir.sital@gmail.com>
2023-02-15 18:08:16 +0545

    feat: type assertion with usestate

--- src/Components/State/User.tsx
+++ src/Components/State/User.tsx
@@ -4,22 +4,23 @@ type AuthUser = {
     email: string;
 };
 function User() {
-    const [user, setUser] = useState<null | AuthUser>(null);
+    // const [user, setUser] = useState<null | AuthUser>(null);
+    const [user, setUser] = useState<AuthUser>({} as AuthUser);
     const handleLogIn = () => {
         setUser({
             name: 'Shishir',
             email: 'shishir@example.com',
         });
     };
-    const handleLogOut = () => {
-        setUser(null);
-    };
+    // const handleLogOut = () => {
+    //     setUser(null);
+    // };
     return (
         <div>
-            <button onClick={handleLogOut}>Logout</button>
+            {/* <button onClick={handleLogOut}>Logout</button> */}
             <button onClick={handleLogIn}>Login</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     );
 }
